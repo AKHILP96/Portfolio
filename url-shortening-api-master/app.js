c49953423dedfddd43b1789708c0d9dc2851e1c7
AKHIL POKLE <akhilpokle96@gmail.com>
2020-07-02 21:11:42 +0530

    nearly done

---
+++ url-shortening-api-master/app.js
@@ -0,0 +1,71 @@
+const form = document.querySelector('form');
+const links = document.querySelector('.generate-link');
+const copied = document.querySelector('.copied');
+
+form.addEventListener('submit', async (e) => {
+	e.preventDefault();
+	let input = e.target.querySelector('input').value;
+	console.log(input);
+	if (input == '' || input == null) {
+		document.querySelector('#error1').style.display = 'inline-block';
+		document.querySelector('#error2').style.display = 'none';
+		return;
+	}
+	const regex = /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/;
+	if (regex.test(input)) {
+		document.querySelector('#error1').style.display = 'none';
+		document.querySelector('#error2').style.display = 'none';
+		let link = input;
+		link = link.trim();
+		let hyperlink = link.slice(0, 3);
+		console.log(hyperlink);
+		if (hyperlink === 'www') {
+			link = 'https://' + link.slice(4);
+		}
+		try {
+			const response = await fetch(`https://rel.ink/api/links/?url=${link}`, {
+				method: 'POST',
+				headers: {
+					'Content-type': 'application/json'
+				},
+				body: JSON.stringify({ url: link })
+			});
+			const json = await response.json();
+			console.log(json);
+			const shortUrl = 'https://rel.ink/' + json.hashid;
+			const longUrl = json.url;
+			console.log(shortUrl);
+			createListItem(longUrl, shortUrl);
+			// generate list
+		} catch (error) {
+			// generate error
+			document.querySelector('#error1').style.display = 'none';
+			document.querySelector('#error2').style.display = 'inline-block';
+		}
+	} else {
+		// generate error
+		document.querySelector('#error1').style.display = 'none';
+		document.querySelector('#error2').style.display = 'inline-block';
+	}
+});
+
+function createListItem(longUrl, shortUrl) {
+	let li = document.createElement('li');
+	li.innerHTML = `<p>${longUrl}</p> <div class="short"><p>${shortUrl}</p><button class="copy">Copy</button></div>`;
+	links.insertBefore(li, links.childNodes[0]);
+}
+
+links.addEventListener('click', (e) => {
+	let link = e.path[0].previousSibling.textContent;
+	let textarea = document.createElement('textarea');
+	textarea.value = link;
+	document.body.appendChild(textarea);
+	textarea.select();
+	document.execCommand('copy');
+	document.body.removeChild(textarea);
+	console.log(copied);
+	copied.style.display = 'flex';
+	copied.style.justifyContent = 'center';
+	copied.style.alignItems = 'center';
+	copied.style.top = window.pageYOffset - document.documentElement.clientHeight / 2;
+});
